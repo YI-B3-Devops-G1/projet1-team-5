9ec16021fe8219779fd06b82691e35c2cb68d5db
NicolasB <nicolas.barbosa@volvo.com>
2020-02-03 15:24:27 +0100

    commit tp2 inside

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get('/', function(req,res){
+    res.json({message:"Hello World"});
+  });
+  
+app.get('/status', function(req,res){
+    res.json({
+        "status": "OK",
+        "postgresUptime": String,
+        "redisConnectedClients": Number
+    });
+  });
+
+
+  app.listen(port)
